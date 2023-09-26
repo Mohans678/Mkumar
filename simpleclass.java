99eeaaf0038d2ba4d9e518c407eaf9fe64573c3e
Mohan <mohans3033@gmail.com>
2023-09-26 16:03:26 +0530

    simpleclass

---
+++ simpleclass.java
@@ -0,0 +1,11 @@
+package test1;
+
+public class simpleclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is my first class");
+
+	}
+
+}
